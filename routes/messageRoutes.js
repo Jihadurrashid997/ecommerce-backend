38b739b189cf606044ce4f8c595e6c5f8c105572
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-04 23:56:43 +0600

    Add message routes for sending and managing messages

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const router = express.Router();
 
+const auth = require("../middleware/auth");
+
+const {
+
+    sendMessage,
+    getConversation,
+    markSeen
+
+} = require("../controllers/messageController");
+
+router.post("/send", auth(), sendMessage);
+
+router.get("/conversation/:userId", auth(), getConversation);
+
+router.put("/seen/:userId", auth(), markSeen);
+
+module.exports = router;
