05479f602c0d96e7ec0f643aaf3bbd5484e9abe4
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-06 00:37:35 +0600

    Format api.js for consistent code style

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-api-9wc9.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "https://ecommerce-api-9wc9.onrender.com/api",
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    const token = localStorage.getItem("token");
 
-  return config;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default api;
