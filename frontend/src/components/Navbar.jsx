d06b9f0ef46eabcdd8db9bbe09a4460f18967883
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-06 00:22:02 +0600

    Refactor Navbar component with routing and state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,69 +1,88 @@
-import React from "react";
-import "./Navbar.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
-  FaSearch,
-  FaBell,
   FaShoppingCart,
+  FaHeart,
   FaComments,
-  FaUserCircle
+  FaUserCircle,
+  FaBars,
+  FaTimes,
+  FaSearch
 } from "react-icons/fa";
 
-function Navbar() {
+import "./Navbar.css";
+
+const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
       <div className="logo">
-        Market<span>Place</span>
+        <Link to="/">Marketplace</Link>
       </div>
 
       <div className="search-box">
+
         <FaSearch className="search-icon" />
+
         <input
           type="text"
           placeholder="Search products..."
         />
-      </div>
-
-      <div className="nav-links">
-
-        <a href="/">Home</a>
-
-        <a href="/products">Products</a>
-
-        <a href="/seller">Seller</a>
-
-        <a href="/orders">Orders</a>
 
       </div>
 
-      <div className="nav-right">
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
 
-        <div className="nav-icon">
-          <FaComments />
-        </div>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
 
-        <div className="nav-icon">
-          <FaBell />
-        </div>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
 
-        <div className="nav-icon">
-          <FaShoppingCart />
-        </div>
+        <li>
+          <Link to="/seller">Seller</Link>
+        </li>
 
-        <div className="profile">
+        <li>
+          <Link to="/messenger">
+            <FaComments />
+          </Link>
+        </li>
 
-          <FaUserCircle className="profile-image" />
+        <li>
+          <Link to="/profile">
+            <FaUserCircle />
+          </Link>
+        </li>
 
-          <span className="profile-name">
-            My Account
-          </span>
+        <li>
+          <Link to="#">
+            <FaHeart />
+          </Link>
+        </li>
 
-        </div>
+        <li>
+          <Link to="#">
+            <FaShoppingCart />
+          </Link>
+        </li>
 
-      </div>
+      </ul>
+
+      <button
+        className="menu-btn"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
 
     </nav>
   );
-}
+};
 
 export default Navbar;
