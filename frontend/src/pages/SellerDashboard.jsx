2e29db03c70f593795e0e5acd8070f3386736608
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-06 20:06:22 +0600

    Update CSS import path in SellerDashboard component

--- frontend/src/pages/SellerDashboard.jsx
+++ frontend/src/pages/SellerDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
-import "./SellerDashboard.css";
+import "../styles/SellerDashboard.css";
 
 const SellerDashboard = () => {
 
