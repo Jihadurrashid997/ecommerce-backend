53046f008868b48fbf53da1e3e78559b8d537113
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-06 10:04:35 +0600

    Refactor root render to include WishlistProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App";
+
+import { AppProvider } from "./context/AppContext";
 import { CartProvider } from "./context/CartContext";
+import { WishlistProvider } from "./context/WishlistContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-
-<React.StrictMode>
-
-<AppProvider>
-
-<CartProvider>
-
-<App/>
-
-</CartProvider>
-
-</AppProvider>
-
-</React.StrictMode>
-
+  <React.StrictMode>
+    <AppProvider>
+      <CartProvider>
+        <WishlistProvider>
+          <App />
+        </WishlistProvider>
+      </CartProvider>
+    </AppProvider>
+  </React.StrictMode>
 );
