16feda3db6d2dc24acccd97a27f9ac052a232df5
Jihadurrashid997 <jihadurrashid997@gmail.com>
2026-08-12 20:09:00 +0600

    Refactor user routes for better readability

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,45 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
     getUsers,
-    deleteUser
+    deleteUser,
+    updateProfile
 } = require("../controllers/userController");
 
 const auth = require("../middleware/auth");
 
+
+// ==========================
+// MY PROFILE
+// ==========================
+
+router.put(
+    "/profile",
+    auth(),
+    updateProfile
+);
+
+
+// ==========================
+// ADMIN
+// ==========================
+
 // Get all users
-router.get("/", auth(["admin"]), getUsers);
+router.get(
+    "/",
+    auth(["admin"]),
+    getUsers
+);
+
 
 // Delete user
-router.delete("/:id", auth(["admin"]), deleteUser);
+router.delete(
+    "/:id",
+    auth(["admin"]),
+    deleteUser
+);
+
 
 module.exports = router;
